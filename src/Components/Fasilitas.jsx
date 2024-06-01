d008b7aa68086c06e8a64f2b896a5c76056e0eef
sandrinaanilia <sandrinaanilia04@gmail.com>
2024-06-01 21:59:10 +0700

    sing penting yakin

--- src/Components/Fasilitas.jsx
+++
@@ -1,9 +0,0 @@
-const Fasilitas = ({image, title, desc})=>{
-    return <div className="facility">
-        <img src= {image} alt={title} />
-        <h3>{title}</h3>
-        <p>{desc}</p>
-    </div>
-}
-
-export default Fasilitas
--- src/store/index.js
+++
@@ -1,8 +0,0 @@
-import {configureStore} from "@reduxjs/toolkit";
-import auth from "./auth.js";
-
-const Store = configureStore({
-    reducer:auth
-})
-
-export default Store
